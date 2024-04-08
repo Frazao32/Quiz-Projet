e7ff30797b54296e0030047b9ab0967d48df014d
L.Frazao <100303033+Frazao32@users.noreply.github.com>
2024-04-08 13:21:12 +0100

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,6 +1,6 @@
-print('Welcome to my countrie's capital Quiz ')
+print('Welcome to my countries capital Quiz ')
 a = input('What is the capital of Brazil ?')
-  if a = 'brasilia':
-      print('Correta resposta')
-      else:
-        print('Resposta Incorreta')
+if a == 'brasilia':
+    print('Correta resposta')
+else:
+    print('Resposta Incorreta')
